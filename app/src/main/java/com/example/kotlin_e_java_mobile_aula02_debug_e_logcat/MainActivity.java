43a66afb8061e02ad75e2af13ef823d33c83b168
LucNath <lucasnathan.marc@gmail.com>
2026-02-17 18:28:28 -0300

    Initial commit - Projeto Aula 02 Debug e LogCat

--- app/src/main/java/com/example/kotlin_e_java_mobile_aula02_debug_e_logcat/MainActivity.java
+++ app/src/main/java/com/example/kotlin_e_java_mobile_aula02_debug_e_logcat/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.kotlin_e_java_mobile_aula02_debug_e_logcat;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnFabricarPessoa;
+    private TextView txtPessoa;
+    Pessoa pessoa;
+    private int idade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnFabricarPessoa = findViewById(R.id.btnFabricarPessoa);
+        txtPessoa = findViewById(R.id.txtPessoa);
+
+        btnFabricarPessoa.setOnClickListener(v -> {
+            Toast.makeText(
+                    this,
+                    "Botão Fabricar Pessoa Clicado",
+                    Toast.LENGTH_SHORT).show();
+
+            idade = getIdade();
+            pessoa = new Pessoa("Lucas", idade);
+            txtPessoa.setText(pessoa.toString());
+        });
+
+    }
+
+    private static int getIdade() {
+        Random idadeAleatoria = new Random();
+        int min = 18;
+        int max = 60;
+        int idade = idadeAleatoria.nextInt((max - min) + 1) + min;
+        return idade;
     }
 }
---
+++ app/src/main/java/com/example/kotlin_e_java_mobile_aula02_debug_e_logcat/Pessoa.java
@@ -0,0 +1,36 @@
+package com.example.kotlin_e_java_mobile_aula02_debug_e_logcat;
+
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade){
+
+        this.nome = nome;
+        this.idade = idade;
+
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString(){
+        return "Nome: " + nome + "\nIdade: " + idade;
+    }
+
+}
