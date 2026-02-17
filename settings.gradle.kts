380a5e08163265ce0a8e313e22f1fd64f5052f4d
LucNath <lucasnathan.marc@gmail.com>
2026-02-16 21:57:45 -0300

    Initial commit - Aula 02 Debug e LogCat

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kotlin-e-Java-Mobile-Aula02-Debug-e-LogCat"
+include(":app")
